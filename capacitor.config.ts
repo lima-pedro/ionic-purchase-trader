570e37141c814f7dea5823eb1288389537f416ea
Pedro Lima <pedro.lima@salveanoiva.com.br>
2021-06-28 22:15:44 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.br.planetatrader.revista',
+  appName: 'ionicPurchase',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
